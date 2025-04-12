b51c0970836cc08792732f96d1e611d0e9e90f5b
[stephanie wong] <[stephaniewong1515@gmail.com]>
2025-04-12 10:37:44 -0700

    add ip

--- rock-climbing-logbook/backend/server.js
+++ rock-climbing-logbook/backend/server.js
@@ -21,6 +21,6 @@ app.use('/graphql', graphqlHTTP({
 }));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
